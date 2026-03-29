9e2bcaff0edc7113d4d0cb974758161c902ae39c
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-03-29 17:09:12 +0200

    Snake 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,6 +3,12 @@ const people = [
   { name: 'Giulia', age: 24 },
   { name: 'Marco', age: 67 }
 ];
+people.forEach((people)=>{
+  const namePeople= people.name
+  console.log(namePeople);
+  
+})
+
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
