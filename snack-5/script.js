f43d0a360da62b0f82ca805cd5fb79fd63b2be41
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-03-29 17:25:54 +0200

    Snake 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,10 @@
 const nums = [2, 8, 4, 7, 12, 87];
+const evensNum=nums.filter((nums)=>{
+    if (nums%2 ===0) {
+        return true
+    }
+})
+console.log(evensNum);
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
