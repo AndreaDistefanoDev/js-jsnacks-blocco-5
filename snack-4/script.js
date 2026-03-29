b729fd984f3a9ffe4286749c6ff62be03f4bbe4a
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-03-29 17:20:09 +0200

    Snake 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,11 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const postsName=posts.map((posts)=>{
+  return posts.author
+})
+console.log(postsName);
+
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
